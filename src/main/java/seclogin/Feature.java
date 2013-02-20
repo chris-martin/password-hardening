66dd98875901a9293c2c0cde2c63806b0532c360
kf83 <Kelsey.Francis@gtri.gatech.edu>
2013-02-20 13:12:53 -0500

    Remove unused methods.

--- src/main/java/seclogin/Feature.java
+++ src/main/java/seclogin/Feature.java
@@ -1,14 +1,3 @@
 package seclogin;
 
-public enum Feature {
-
-    ALPHA, BETA;
-
-    public static byte asByte(Feature feature) {
-        return feature == null ? 0 : (byte) (feature.ordinal() + 1);
-    }
-
-    public static Feature fromByte(byte b) {
-        return b == 0 ? null : values()[b - 1];
-    }
-}
+public enum Feature { ALPHA, BETA }
